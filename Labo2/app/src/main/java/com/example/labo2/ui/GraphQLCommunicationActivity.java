9b9d1836115dd5de17166ccf6c61b5c8c7489226
RengokuryuuhonokaCrimsonFlame <julien.rod@heig-vd.ch>
2019-11-08 14:55:01 +0100

    début d'application

--- Labo2/app/src/main/java/com/example/labo2/ui/GraphQLCommunicationActivity.java
+++ Labo2/app/src/main/java/com/example/labo2/ui/GraphQLCommunicationActivity.java
@@ -1,6 +1,133 @@
 package com.example.labo2.ui;
 
 import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.labo2.R;
+import com.example.labo2.ui.eventListener.CommunicationEventListener;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 public class GraphQLCommunicationActivity extends Activity {
+
+    Spinner selectList = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_async);
+        this.selectList = findViewById(R.id.spinner);
+        SymComManager scm = new SymComManager();
+        scm.setCommunicationEventListener(
+                response -> {
+                    // Code de traitement de la réponse – dans le UI-Thread
+                    if(response != null){
+                        SpinnerAdapter<String> adapter = new RecyclerView.Adapter<String>();
+                        selectList.setAdapter(adapter);
+                        return true;
+                    }
+                    return false;
+                });
+        try {
+            scm.sendRequest("{allAuthors{id first_name last_name}}",
+                    "http://sym.iict.ch/api/graphql");
+        } catch (Exception e) {
+            System.out.println("Exception : " + e);
+            e.printStackTrace();
+        }
+
+
+        spinner.setOnClickListener((v) -> { //{"query":"{allPostByAuthor(authorId: 1){title description}}"}
+            SymComManager scm = new SymComManager();
+            scm.setCommunicationEventListener(
+                    response -> {
+                        // Code de traitement de la réponse – dans le UI-Thread
+                        if(response != null){
+                            reponse.setText(response);
+                            return true;
+                        }
+                        return false;
+                    });
+            try {
+                scm.sendRequest(message.getText().toString(), "http://sym.iict.ch/rest/txt");
+            } catch (Exception e) {
+                System.out.println("Exception : " + e);
+                e.printStackTrace();
+            }
+            message.setText("");
+        });
+
+        retour.setOnClickListener((v) -> finish());
+    }
+
+    public class SymComManager extends AsyncTask<String, Void, String> {
+
+        private CommunicationEventListener cel = null;
+
+        @Override
+        protected String doInBackground(String... strings) {
+            URL obj;
+            try {
+                obj = new URL(strings[1]);
+                HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Request", strings[0]);
+                connection.setDoOutput(true);
+                BufferedWriter os = new BufferedWriter(new OutputStreamWriter(
+                        connection.getOutputStream(), "UTF-8"));
+                os.append(strings[0]);
+                os.close();
+                BufferedReader in = new BufferedReader(new InputStreamReader(
+                        connection.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                return response.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // print result
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            cel.handleServerResponse(result);
+        }
+
+        /**
+         * Permet d'envoyer un document request vers le serveur désigné par ur
+         * @param request Le texte mis
+         * @param url L'URL du serveur à joindre
+         */
+        void sendRequest(String request, String url) {
+            this.execute(request, url);
+        }
+
+        void setCommunicationEventListener (CommunicationEventListener l){
+            this.cel = l;
+        }
+    }
 }
