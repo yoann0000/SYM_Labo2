1d6943c283c7f500a8a7b10d14b6758485ddcae2
Loris <crullloris@gmail.com>
2019-11-14 20:32:51 +0100

    Fixed JSON and XML

--- Labo2/app/src/main/java/com/example/labo2/ui/ObjectCommunicationActivity.java
+++ Labo2/app/src/main/java/com/example/labo2/ui/ObjectCommunicationActivity.java
@@ -23,7 +23,9 @@ import org.xmlpull.v1.XmlSerializer;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.StringReader;
 import java.io.StringWriter;
@@ -37,20 +39,29 @@ public class ObjectCommunicationActivity extends Activity {
 
     public class Person {
         private String name;
+        private String firstname;
+        private String middlename;
+        private String gender;
         private String phone;
 
-        Person(String name, String phone) {
+        Person(String name, String firstname, String middlename, String gender, String phone) {
             this.name = name;
+            this.firstname = firstname;
+            this.middlename = middlename;
+            this.gender = gender;
             this.phone = phone;
         }
 
         Person(Person p) {
             this.name = p.name;
+            this.firstname = p.firstname;
+            this.middlename = p.middlename;
+            this.gender = p.gender;
             this.phone = p.phone;
         }
 
         public String toString() {
-            return this.name + " " + this.phone;
+            return this.firstname + " " + this.middlename + " " + this.name + " [" + this.gender + "]\n" + this.phone;
         }
     }
 
@@ -111,15 +122,26 @@ public class ObjectCommunicationActivity extends Activity {
                         return false;
                     });
             try {
+                String completeName = this.name.getText().toString();
+                String[] names = completeName.split(" ");
+
+                String name = names.length >= 2 ? completeName.substring(completeName.indexOf(" ") + 1) : "";
+                String firstname = names.length >= 1 ? names[0] : "";
+                String middlename = "The Rock"; // Défini arbitrairement
+                String gender = "m"; // Défini arbitrairement
+                String phone = this.phone.getText().toString();
+
                 switch(radioGroup.getCheckedRadioButtonId()) {
                     case R.id.jsonBtn:
-                        Person person = new Person(this.name.getText().toString(), this.phone.getText().toString());
-                        scm.sendRequest(new Gson().toJson(person), "http://sym.iict.ch/rest/json", "application/json");
+                        Person person = new Person(name, firstname, middlename, gender, phone);
+                        scm.sendRequest(new Gson().toJson(person),
+                                "http://sym.iict.ch/rest/json", "application/json");
                         break;
 
                     case R.id.xmlBtn:
-                        String xmlRequest = getXmlRequest(this.name.getText().toString(), this.phone.getText().toString());
-                        scm.sendRequest(xmlRequest, "http://sym.iict.ch/rest/xml", "application/xml");
+                        String xmlRequest = getXmlRequest(name, firstname, middlename, gender, phone);
+                        scm.sendRequest(xmlRequest,
+                                "http://sym.iict.ch/rest/xml", "application/xml");
                         break;
 
                     default:
@@ -135,41 +157,62 @@ public class ObjectCommunicationActivity extends Activity {
         back.setOnClickListener((v) -> finish());
     }
 
-    public String getXmlRequest(String name, String phone) {
-        XmlSerializer xmlData = Xml.newSerializer();
-        StringWriter xml = new StringWriter();
+    public String getXmlRequest(String name, String firstname, String middlename, String gender, String phone) {
+        XmlSerializer serializer = Xml.newSerializer();
+        StringWriter sw = new StringWriter();
+
         try {
-            xmlData.setOutput(xml);
-            xmlData.startDocument("UTF-8", null);
-            xmlData.docdecl(" directory SYSTEM \"http://sym.iict.ch/directory.dtd\"");
-            xmlData.startTag("","directory");
-            xmlData.startTag("", "person");
+            serializer.setOutput(sw);
+            serializer.startDocument("UTF-8", null);
+            serializer.docdecl(" directory SYSTEM \"http://sym.iict.ch/directory.dtd\"");
+            serializer.startTag("","directory");
+            serializer.startTag("", "person");
 
             // Name
-            xmlData.startTag("", "name");
-            xmlData.text(name);
-            xmlData.endTag("", "name");
+            serializer.startTag("", "name");
+            serializer.text(name);
+            serializer.endTag("", "name");
+
+            // Firstname
+            serializer.startTag("", "firstname");
+            serializer.text(firstname);
+            serializer.endTag("", "firstname");
+
+            // Middlename
+            serializer.startTag("", "middlename");
+            serializer.text(middlename);
+            serializer.endTag("", "middlename");
+
+            // Gender
+            serializer.startTag("", "gender");
+            serializer.text(gender);
+            serializer.endTag("", "gender");
 
             // Phone
-            xmlData.startTag("", "phone");
-            xmlData.text(phone);
-            xmlData.endTag("", "phone");
+            serializer.startTag("", "phone");
+            serializer.attribute("", "type", "mobile");
+            serializer.text(phone);
+            serializer.endTag("", "phone");
 
-            xmlData.endTag("", "person");
-            xmlData.endTag("", "directory");
-            xmlData.endDocument();
+            serializer.endTag("", "person");
+            serializer.endTag("", "directory");
+            serializer.endDocument();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return xml.toString();
+        return sw.toString();
     }
 
     public Person parseXml(String xml) {
-        String name = null;
-        String phone = null;
+        String name = "";
+        String firstname = "";
+        String middlename = "";
+        String gender = "";
+        String phone = "";
         XmlPullParserFactory xppf;
+
         try {
             xppf = XmlPullParserFactory.newInstance();
             XmlPullParser xpp = xppf.newPullParser();
@@ -182,6 +225,18 @@ public class ObjectCommunicationActivity extends Activity {
                     if (tag.equals("name")) {
                         if (xpp.next() == XmlPullParser.TEXT) {
                             name = xpp.getText();
+                        }
+                    } else if (tag.equals("firstname")) {
+                        if (xpp.next() == XmlPullParser.TEXT) {
+                            firstname = xpp.getText();
+                        }
+                    } else if (tag.equals("middlename")) {
+                        if (xpp.next() == XmlPullParser.TEXT) {
+                            middlename = xpp.getText();
+                        }
+                    } else if (tag.equals("gender")) {
+                        if (xpp.next() == XmlPullParser.TEXT) {
+                            gender = xpp.getText();
                         }
                     } else if (tag.equals("phone")) {
                         if (xpp.next() == XmlPullParser.TEXT) {
@@ -197,7 +252,7 @@ public class ObjectCommunicationActivity extends Activity {
             e.printStackTrace();
         }
 
-        return new Person(name, phone);
+        return new Person(name, firstname, middlename, gender, phone);
     }
 
     public class SymComManager extends AsyncTask<String, Void, String> {
@@ -215,11 +270,13 @@ public class ObjectCommunicationActivity extends Activity {
                 connection.setRequestProperty("Request", strings[0]);
                 connection.setRequestProperty("Content-Type", strings[2]);
                 connection.setRequestProperty("Accept", strings[2]);
+
                 connection.setDoOutput(true);
                 BufferedWriter os = new BufferedWriter(new OutputStreamWriter(
                         connection.getOutputStream(), "UTF-8"));
                 os.append(strings[0]);
                 os.close();
+
                 BufferedReader in = new BufferedReader(new InputStreamReader(
                         connection.getInputStream()));
                 String inputLine;
@@ -229,8 +286,10 @@ public class ObjectCommunicationActivity extends Activity {
                     response.append(inputLine);
                 }
                 in.close();
+                connection.disconnect();
                 System.out.println(response.toString());
                 return response.toString();
+
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (ProtocolException e) {
